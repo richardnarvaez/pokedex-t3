74f68307ebe489ac452f4df4d5c7d896bbb8f8bf
Richard Vinueza <richard.bvn997@gmail.com>
2024-12-14 12:50:12 -0500

    chore: Add ChevronIcon and SearchIcon components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
 import { type NextPage } from "next";
 import Head from "next/head";
 import { useRouter } from "next/router";
-import { useState, useEffect } from "react";
+import { useState, useEffect, Fragment } from "react";
 import { api } from "../utils/pokemon.api";
 import PokemonCard from "../components/pokemon.item";
-import { ChevronIcon } from "../components/chevron.icon";
+import { ChevronIcon } from "../components/Icons/chevron.icon";
+import { SearchIcon } from "../components/Icons/search.icon";
 
 const POKEMON_LENGTH_GENERATIONS = 9;
 const POKEMON_TYPES = [
@@ -42,10 +43,10 @@ const Home: NextPage = () => {
     isFetchingNextPage,
     isLoading,
     isFetching,
-    error
+    error,
   } = api.pokemon.getPokemons.useInfiniteQuery(
     {
-      limit: 25,
+      limit: 100,
       search: debouncedSearch,
       type: selectedType,
       generation: selectedGen,
@@ -53,7 +54,7 @@ const Home: NextPage = () => {
     {
       getNextPageParam: (lastPage) => lastPage.nextCursor,
       keepPreviousData: true,
-    }
+    },
   );
 
   useEffect(() => {
@@ -91,17 +92,18 @@ const Home: NextPage = () => {
   // Infinite Scroll
   useEffect(() => {
     const onScroll = () => {
-      const { scrollHeight, scrollTop, clientHeight } = document.documentElement;
+      const { scrollHeight, scrollTop, clientHeight } =
+        document.documentElement;
       const isNearBottom = scrollHeight - scrollTop <= clientHeight * 1.5;
-      
+
       if (isNearBottom && hasNextPage && !isFetchingNextPage) {
-        console.log('Fetching next page...');
+        console.log("Fetching next page...");
         void fetchNextPage();
       }
     };
 
-    window.addEventListener('scroll', onScroll);
-    return () => window.removeEventListener('scroll', onScroll);
+    window.addEventListener("scroll", onScroll);
+    return () => window.removeEventListener("scroll", onScroll);
   }, [hasNextPage, isFetchingNextPage, fetchNextPage]);
 
   if (error) {
@@ -111,7 +113,7 @@ const Home: NextPage = () => {
       </div>
     );
   }
-  
+
   return (
     <>
       <Head>
@@ -125,76 +127,91 @@ const Home: NextPage = () => {
             Poké<span className="text-blue-600">dex</span>
           </h1>
 
-          <div className="mb-8 flex flex-col gap-4 md:flex-row md:justify-between">
-            <input
-              type="text"
-              placeholder="Buscar Pokémon..."
-              className="h-10 rounded-md border border-gray-200 bg-white px-3 py-2 text-sm ring-offset-white placeholder:text-gray-500 focus:outline-none focus:ring-2 focus:ring-gray-950 focus:ring-offset-2"
-              value={search}
-              onChange={(e) => {
-                setSearch(e.target.value);
-                updateFilters(e.target.value, selectedType, selectedGen);
-              }}
-            />
+          <div className="sticky top-0 z-50 mb-2 flex flex-col gap-4 border-b bg-white py-4 md:flex-row md:justify-between">
+            <div className="relative">
+              <input
+                type="text"
+                placeholder="Buscar Pokémon..."
+                className="h-10 rounded-md border border-gray-200 bg-white py-2 pl-12 pr-4 text-sm ring-offset-white placeholder:text-gray-500 focus:outline-none focus:ring-2 focus:ring-gray-950 focus:ring-offset-2"
+                value={search}
+                onChange={(e) => {
+                  setSearch(e.target.value);
+                  updateFilters(e.target.value, selectedType, selectedGen);
+                }}
+              />
+              <span className="absolute left-3 top-1/2 -translate-y-1/2 border-r pr-2 text-gray-500">
+                <SearchIcon width={18} height={18} />
+              </span>
+            </div>
             <div className="flex gap-4">
               <div className="relative">
                 <select
+                  title="Filtrar por tipo"
                   value={selectedType}
-                onChange={(e) => {
-                  setSelectedType(e.target.value);
-                  updateFilters(search, e.target.value, selectedGen);
-                }}
-                className="h-10 w-[200px] appearance-none rounded-md border border-gray-200 bg-white px-3 py-2 text-sm ring-offset-white focus:outline-none focus:ring-2 focus:ring-gray-950 focus:ring-offset-2"
-              >
-                <option value="">Todos los tipos</option>
-                {POKEMON_TYPES.map((type) => (
-                  <option key={type} value={type}>
-                    {type.charAt(0).toUpperCase() + type.slice(1)}
-                  </option>
-                ))}
-              </select>
-              <span className="pointer-events-none absolute right-3 top-1/2 -translate-y-1/2 text-gray-500">
-              <ChevronIcon />
-              </span>
-            </div>
-
-            <div className="relative">
-              <select
-                value={selectedGen}
-                onChange={(e) => {
-                  setSelectedGen(e.target.value);
-                  updateFilters(search, selectedType, e.target.value);
-                }}
-                className="h-10 w-[200px] appearance-none rounded-md border border-gray-200 bg-white px-3 py-2 text-sm ring-offset-white focus:outline-none focus:ring-2 focus:ring-gray-950 focus:ring-offset-2"
+                  onChange={(e) => {
+                    setSelectedType(e.target.value);
+                    updateFilters(search, e.target.value, selectedGen);
+                  }}
+                  className="h-10 w-[200px] appearance-none rounded-md border border-gray-200 bg-white px-3 py-2 text-sm ring-offset-white focus:outline-none focus:ring-2 focus:ring-gray-950 focus:ring-offset-2"
                 >
-                <option value="">Todas las generaciones</option>
-                {Array.from({ length: POKEMON_LENGTH_GENERATIONS }, (_, i) => (
-                  <option key={i + 1} value={String(i + 1)}>
-                    Generación {i + 1}
-                  </option>
-                ))}
-              </select>
-              <span className="pointer-events-none absolute right-3 top-1/2 -translate-y-1/2 text-gray-500">
-              <ChevronIcon />
-              </span>
-            </div>
+                  <option value="">Todos los tipos</option>
+                  {POKEMON_TYPES.map((type) => (
+                    <option key={type} value={type}>
+                      {type.charAt(0).toUpperCase() + type.slice(1)}
+                    </option>
+                  ))}
+                </select>
+                <span className="pointer-events-none absolute right-3 top-1/2 -translate-y-1/2 text-gray-500">
+                  <ChevronIcon />
+                </span>
+              </div>
+
+              <div className="relative">
+                <select
+                  title="Filtrar por generación"
+                  value={selectedGen}
+                  onChange={(e) => {
+                    setSelectedGen(e.target.value);
+                    updateFilters(search, selectedType, e.target.value);
+                  }}
+                  className="h-10 w-[232px] appearance-none rounded-md border border-gray-200 bg-white px-3 py-2 text-sm ring-offset-white focus:outline-none focus:ring-2 focus:ring-gray-950 focus:ring-offset-2"
+                >
+                  <option value="">Todas las generaciones</option>
+                  {Array.from(
+                    { length: POKEMON_LENGTH_GENERATIONS },
+                    (_, i) => (
+                      <option key={i + 1} value={String(i + 1)}>
+                        Generación {i + 1}
+                      </option>
+                    ),
+                  )}
+                </select>
+                <span className="pointer-events-none absolute right-3 top-1/2 -translate-y-1/2 text-gray-500">
+                  <ChevronIcon />
+                </span>
+              </div>
             </div>
           </div>
 
-          <div className="flex justify-center p-4 h-12">
+          <div className="mb-2 flex h-6 justify-center">
             {isFetching && (
               <div className="h-6 w-6 animate-spin rounded-full border-2 border-gray-200 border-t-gray-900" />
             )}
           </div>
+
           {isLoading ? (
             <div className="text-center text-2xl text-gray-800">
               Cargando Pokémon...
             </div>
           ) : (
-            <div className="grid grid-cols-1 gap-6 md:grid-cols-3 lg:grid-cols-4">
-              {data?.pages.flatMap((page) => page.items).map((pokemon) => (
-                <>{pokemon?.id && <PokemonCard key={pokemon.id} pokemon={pokemon} />}</>
-              ))}
+            <div className="grid grid-cols-1 gap-6 px-2 md:grid-cols-3 lg:grid-cols-4">
+              {data?.pages
+                .flatMap((page) => page.items)
+                .map((pokemon) => (
+                  <Fragment key={pokemon?.id}>
+                    {pokemon?.id && <PokemonCard pokemon={pokemon} />}
+                  </Fragment>
+                ))}
               {data?.pages[0]?.items.length === 0 && (
                 <div className="col-span-full text-center text-xl text-gray-800">
                   No se encontraron Pokémon con los filtros seleccionados
